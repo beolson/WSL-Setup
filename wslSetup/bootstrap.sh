d4f2908b9014c798560ef1e3bef805ed839b4d76
Brian Olson <brian.olson@kiewit.com>
2023-06-19 16:06:08 -0500

    support certs

--- wslSetup/bootstrap.sh
+++ wslSetup/bootstrap.sh
@@ -2,7 +2,6 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install ansible -y
-# echo ~
-# ls -lA ~
-# export SSL_CERT_DIR=/usr/local/share/ca-certificates/
-ansible-playbook -K ./wslSetup/playbook.yaml
+
+
+ansible-playbook -K ./wslSetup/playbook.yaml 
