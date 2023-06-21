5235f9e991afbb7beaaa6bf647d10987f194e9db
Brian Olson <beolson@hero4hire.com>
2023-06-20 20:20:36 -0500

    add script to setup windows

---
+++ setup_win.ps1
@@ -0,0 +1,8 @@
+winget install --id=Microsoft.PowerShell -e  ; 
+winget install --id=Microsoft.AzureDataStudio -e  ; 
+winget install --id=Docker.DockerDesktop -e  ; 
+winget install --id=Microsoft.VisualStudioCode -e  ; 
+winget install --id=Git.Git -e  ; 
+winget install -e --id Google.Chrome ;
+winget install -e --id Notepad++.Notepad++ ;
+winget install -e --id Microsoft.WindowsTerminal ; 
