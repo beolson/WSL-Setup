16600e8ccd8c154927e52a50c09d578ee80ec04e
Brian Olson <brian.olson@kiewit.com>
2024-11-07 08:46:20 -0600

    update

--- Old-Ansible/install_dist.ps1
+++
@@ -1,5 +0,0 @@
-$json = Get-Content -Raw -Path .\config.json | ConvertFrom-Json
-$distributionName = $json.wslDistribution
-
-wsl --unregister $distributionName
-wsl --install -d $distributionName 
--- bootstrap_step_2.sh
+++ bootstrap_step_2.sh
@@ -11,27 +11,4 @@ echo '****************Setup Complete**************'
 echo '********************************************'
 echo '********************************************'
 
-# Setup Git Credentials manage
-# Configure Git Credentials Manage
-# Setup Git
-# Setup git config
-
-# Setup OhMyBash
-# Config OhMyBash
-
-# if ! grep -qF "/mnt/dev" /etc/fstab; then
-#   echo "/dev/sda1 /mnt/dev ext4 defaults 0 0" | sudo tee -a /etc/fstab
-# fi
-
-# if [ -d "./certs" ]; then
-# sudo dd of=/etc/profile.d/gitconfig.sh << EOF
-# export GCM_CREDENTIAL_STORE=cache
-# export GCM_AZREPOS_CREDENTIALTYPE="oauth"
-# EOF
-# fi
-
-
-
-#https://stevenrbaker.com/tech/managing-dotfiles-with-gnu-stow.html
-#https://www.jakewiesler.com/blog/managing-dotfiles
-#https://stackoverflow.com/questions/28185913/how-to-read-and-parse-the-json-file-and-add-it-into-the-shell-script-variable
+exit
