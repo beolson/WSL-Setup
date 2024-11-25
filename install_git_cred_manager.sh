7ede04f88353f956d6319b2011285fc37d316125
Brian Olson <beolson@hero4hire.com>
2024-11-25 12:13:13 -0600

    update

--- install_git_cred_manager.sh
+++ install_git_cred_manager.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -i
 
 echo '******************* install Git Credential Manager starting'
-
+ARCH=$(uname -m)
+echo $ARCH
 
 if [[ "$ARCH" == "x86_64" ]]; then
     readonly GIT_CREDENTIAL_MANAGER_URL='https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.5.0/gcm-linux_amd64.2.5.0.deb';
     readonly GIT_CREDENTIAL_MANAGER_CHECKSUM='d50ed45408fc76ae82bced4025514987f8c8e9e7c2ab9f4bf57ccbc854d68b3e  gcm.deb'
 elif [[ "$ARCH" == "arm"* ]]; then
     readonly GIT_CREDENTIAL_MANAGER_URL='https://github.com/beolson/git-credential-manager/releases/download/ARM/gcm-linux-arm64.2.6.0.deb';
-    readonly GIT_CREDENTIAL_MANAGER_CHECKSUM='d50ed45408fc76ae82bced4025514987f8c8e9e7c2ab9f4bf57ccbc854d68b3e  gcm.deb'
+    readonly GIT_CREDENTIAL_MANAGER_CHECKSUM='0bd5f5d1e1fdd142933ecf315b39acb90f116d52a9a82f81e2d9301cb75fbbd0  gcm.deb'
+elif [[ "$ARCH" == "aarch64" ]]; then
+    readonly GIT_CREDENTIAL_MANAGER_URL='https://github.com/beolson/git-credential-manager/releases/download/ARM/gcm-linux-arm64.2.6.0.deb';
+    readonly GIT_CREDENTIAL_MANAGER_CHECKSUM='0bd5f5d1e1fdd142933ecf315b39acb90f116d52a9a82f81e2d9301cb75fbbd0  gcm.deb'
 else
     echo 
     return "Unsupported architecture: $ARCH" 2>/dev/null || exit "Unsupported architecture: $ARCH"
