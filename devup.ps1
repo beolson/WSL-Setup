417934018a19f7685e0ad9aed4b9ba62efde436c
Brian Olson <brian.olson@kiewit.com>
2025-01-31 11:45:27 -0600

    update

--- devup.ps1
+++ devup.ps1
@@ -6,21 +6,30 @@ wsl --set-version 2
 wsl --unregister $distributionName
 wsl --install -d $distributionName 
 
+$crt = Get-ChildItem -path 'Cert:\*d72f47d87420e3f0f9bdcac6f03a566743c481b9' -Recurse 
+
+if ($crt -ne $null){
+    Remove-Item certs\zscaler.crt
+    Export-Certificate -Cert $crt[0] -FilePath certs\zscaler.cer
+    certutil -encode certs\zscaler.cer certs\zscaler.pem
+    Rename-Item -Path certs\zscaler.pem -NewName zscaler.crt
+    Remove-Item certs\zscaler.cer
+}
+
+
 
 
 $scripts = @(
     'apt_upgrade.sh'
-    'configure_ca_certs.sh'
     'install_wsl_tools.sh'
     'install_git_cred_manager.sh'
     'install_oh_my_bash.sh'
-  
-    # 'install_node_tools.sh'
-    # 'install_dotnet.sh'
-    # 'install_rust.sh'
-    # 'install_go.sh'
-     'configure_dot_files.sh'
+    'configure_dot_files.sh'
 )
+
+if ($crt -ne $null){
+    $scripts = ,"configure_ca_certs.sh" + $scripts
+}
 #concat the file instead, then run one file?  fix the sudo password problem?
 # https://stackoverflow.com/questions/8749929/how-do-i-concatenate-two-text-files-in-powershell
 
