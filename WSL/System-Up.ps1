18f47af6d78a5bdebda0dfc5e4c89e7f032a25f8
Brian Olson <beolson@hero4hire.com>
2025-05-20 19:26:41 -0500

    update

---
+++ WSL/System-Up.ps1
@@ -0,0 +1,29 @@
+
+param (
+    [switch] $replace = $false
+)
+Import-Module -Name .\ps_modules\Invoke-WSLScript -Verbose
+
+$distributionName = "Debian"
+$userName = "beolson"
+
+if ($replace) {
+    write-host "Replacing existing distro"
+    wsl --set-version 2
+    wsl --unregister $distributionName
+    wsl --install -d $distributionName
+
+    wsl -d $distributionName
+}
+
+#system 
+Invoke-WSLScript -scriptPath './apt_upgrade.sh' -distributionName $distributionName
+Invoke-WSLScript -scriptPath './configure_ca_certs.sh' -distributionName $distributionName
+Invoke-WSLScript -scriptPath './install_git_cred_manager.sh' -distributionName $distributionName
+
+#user
+Invoke-WSLScript -scriptPath './configure_git.sh'-distributionName $distributionName -user $userName
+
+
+
+
